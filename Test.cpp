82d0bf68b7afe6ae8f5d5c6d174ff2e638b15a57
benit1 <benit1234@gmail.com>
2023-05-16 14:19:26 +0300

    Test updated

--- Test.cpp
+++ Test.cpp
@@ -14,59 +14,72 @@
 
 using namespace ariel;
 
-TEST_CASE("Test Cowboy Class") {
-    // Create a new Cowboy
-    Point cowboy_pos(10, 19);
-    Cowboy Lucky_Luke("Lucky Luke", cowboy_pos);
-    // Check if 'Lucky Luke' was initialized correctly
-    CHECK(Lucky_Luke.getName() == "Lucky Luke");
-    CHECK_EQ(Lucky_Luke.getLife(), 110);
-    CHECK(Lucky_Luke.isAlive());
-    CHECK(Lucky_Luke.hasbullets());
-    // Create another Cowboy
-    Point cowboy_pos2(1, 1);
-    Cowboy yuval("yuval tibi", cowboy_pos2);
-    // Lucky Luke shoots at yuval
-    Lucky_Luke.shoot(&yuval);
-    // Check yuval's life after shooting
-    CHECK_EQ(yuval.getLife(), 100);
-    // Lucky Luke shoots until he has no bullets left
-    for (int i = 0; i < 5; i++) {
-        Lucky_Luke.shoot(&yuval);
-    }
-    // Check that Lucky Luke has no bullets left
-    CHECK_FALSE(Lucky_Luke.hasbullets());
-    // Check yuval's life after being shot multiple times
-    CHECK_EQ(yuval.getLife(), 50);
-    // Check if Lucky Luke have bullets after reloading
-    Lucky_Luke.reload();
-    CHECK(Lucky_Luke.hasbullets());
+TEST_CASE("Cowboy Class Initialization tests") {
+// Create a new Cowboy
+    Point luckyLukePos(10, 19);
+    Cowboy luckyLuke("Lucky Luke", luckyLukePos);
+// Check if 'Lucky Luke' was initialized correctly
+    CHECK(luckyLuke.getName() == "Lucky Luke");
+    CHECK_EQ(luckyLuke.getLife(), 110);
+    CHECK(luckyLuke.isAlive());
+    CHECK(luckyLuke.hasbullets());
 }
 
-TEST_CASE("Test Ninja Class") {
+TEST_CASE("Cowboy Class Shooting tests") {
+    Point luckyLukePos(10, 19);
+    Cowboy luckyLuke("Lucky Luke", luckyLukePos);
+    Point yuvalPos(1, 1);
+    Cowboy yuval("yuval tibi", yuvalPos);
+// Lucky Luke shoots at yuval
+    luckyLuke.shoot(&yuval);
+// Check yuval's life after shooting
+    CHECK_EQ(yuval.getLife(), 100);
+// Lucky Luke shoots until he has no bullets left
+    for (int i = 0; i < 5; i++) {
+        luckyLuke.shoot(&yuval);
+    }
+// Check that Lucky Luke has no bullets left
+    CHECK_FALSE(luckyLuke.hasbullets());
+// Check yuval's life after being shot multiple times
+    CHECK_EQ(yuval.getLife(), 50);
+// Check if Lucky Luke have bullets after reloading
+    luckyLuke.reload();
+    CHECK(luckyLuke.hasbullets());
+}
+
+TEST_CASE("Ninja Class Initialization tests") {
 //create new ninjas
-    Point Goten_pos(10, 19);
-    Point Kakaroto_pos(10, 19);
-    Point HattoriHanzo_pos(40, 50);
-    YoungNinja Goten("Goten", Goten_pos);
-    TrainedNinja Kakaroto("Kakaroto", Kakaroto_pos);
-    OldNinja Hattori_Hanzo("Hattori Hanzo", HattoriHanzo_pos);
+    Point gotenPos(10, 19);
+    Point kakarotoPos(10, 19);
+    Point hattoriHanzoPos(40, 50);
+    YoungNinja goten("Goten", gotenPos);
+    TrainedNinja kakaroto("Kakaroto", kakarotoPos);
+    OldNinja hattoriHanzo("Hattori Hanzo", hattoriHanzoPos);
 // Check if the ninjas was initialized correctly
-    CHECK(Goten.getName() == "Goten");
-    CHECK_EQ(Goten.getLife(), 100);
-    CHECK_EQ(Goten.getSpeed(), 14);
-    CHECK(Kakaroto.getName() == "Kakaroto");
-    CHECK_EQ(Kakaroto.getLife(), 120);
-    CHECK_EQ(Kakaroto.getSpeed(), 12);
-    CHECK(Hattori_Hanzo.getName() == "Hattori Hanzo");
-    CHECK_EQ(Hattori_Hanzo.getLife(), 150);
-    CHECK_EQ(Hattori_Hanzo.getSpeed(), 8);
+    CHECK(goten.getName() == "Goten");
+    CHECK_EQ(goten.getLife(), 100);
+    CHECK_EQ(goten.getSpeed(), 14);
+    CHECK(kakaroto.getName() == "Kakaroto");
+    CHECK_EQ(kakaroto.getLife(), 120);
+    CHECK_EQ(kakaroto.getSpeed(), 12);
+    CHECK(hattoriHanzo.getName() == "Hattori Hanzo");
+    CHECK_EQ(hattoriHanzo.getLife(), 150);
+    CHECK_EQ(hattoriHanzo.getSpeed(), 8);
+}
+
+TEST_CASE("Ninja Class Slashing tests") {
+    Point gotenPos(10, 19);
+    Point kakarotoPos(10, 19);
+    Point hattoriHanzoPos(40, 50);
+    YoungNinja goten("Goten", gotenPos);
+    TrainedNinja kakaroto("Kakaroto", kakarotoPos);
+    OldNinja hattoriHanzo("Hattori Hanzo", hattoriHanzoPos);
 //Check Goten's life after being slashed
-    Kakaroto.slash(&Goten);
-    CHECK_EQ(Goten.getLife(), 60);
+    kakaroto.slash(&goten);
+    CHECK_EQ(goten.getLife(), 60);
 //Check Kakaroto's life after being slashed
-    Hattori_Hanzo.slash(&Kakaroto);
-    CHECK_EQ(Kakaroto.getLife(), 120);
+    hattoriHanzo.slash(&kakaroto);
+    CHECK_EQ(kakaroto.getLife(), 120);
 }
 
 TEST_CASE("Test Team Class") {
@@ -90,6 +103,27 @@ TEST_CASE("Test Team Class") {
     }
     CHECK_EQ(team.stillAlive(), 1);
 }
+
+TEST_CASE("Test point Class: Distance method") {
+//Create new points
+    Point p1{3, 3};
+    Point p2{5, 7};
+    Point p3{-2, 2};
+    Point p4{2, -8};
+    Point p5{-2, -2};
+// Check the distance from points with the same distance
+    CHECK_EQ(p1.distance(p2), p2.distance(p1));
+    CHECK_EQ(p3.distance(p4), p4.distance(p3));
+    CHECK_EQ(p5.distance(p1), p1.distance(p5));
+// Test the accuracy of the distance method
+    CHECK_EQ(p4.distance(p1), 11);
+    CHECK_EQ(p2.distance(p5), 7);
+    CHECK_EQ(p1.distance(p1), 0);
+}
+
+
+
+
 
 
 
